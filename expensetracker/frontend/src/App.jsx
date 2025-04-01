aa146ec8cf1c64bddf2bb42641ad3e3ae6e0327a
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2025-04-01 18:08:55 +0530

    heder

--- expensetracker/frontend/src/App.jsx
+++ expensetracker/frontend/src/App.jsx
@@ -7,10 +7,7 @@ import MainPage from "./pages/MainPage/index.jsx";
 import HomePage from "./pages/HomePage";
 import ExpenseNotes from "./components/ExpenseNotes";
 import ExpenseAnalysis from "./components/Analysis/index.jsx";
-// import ExpenseEntry from "./components/ExpenseNotes";
-import { useState } from "react";
 import Footer from "./components/Footer/Footer.jsx";
-// import MainHeader from "./components/MainHeader";
 function App() {
   // const [expenses, setExpenses] = useState([]);
   return (
