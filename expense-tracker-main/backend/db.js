0c7681649713283305dd00a42edfbee4e1d7286e
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2025-07-01 14:00:33 +0530

    sd

--- expense-tracker-main/backend/db.js
+++ expense-tracker-main/backend/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  port: 5432,
   ssl: {
     rejectUnauthorized: false,
   },
